12f440114afd47b4c22eda618c131ba207073d52
phatakmohini@gmail.com <phatakmohini@gmal.com>
2023-05-11 14:49:49 +0530

    s3 created

--- infra_setup/securitygroups.tf
+++ infra_setup/securitygroups.tf
@@ -26,61 +26,61 @@
 # }
 
 
-# sec group from ALB
-resource "aws_security_group" "alb_jenkinsterraform" {
-  name        = "ALB sec group automation"
-  description = "to allow traffic from outside on 80"
-  # vpc_id      = main.aws_cloudformation_export.vpc_id.value
+# # sec group from ALB
+# resource "aws_security_group" "alb_jenkinsterraform" {
+#   name        = "ALB sec group automation"
+#   description = "to allow traffic from outside on 80"
+#   # vpc_id      = main.aws_cloudformation_export.vpc_id.value
 
 
-#incoming
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-#outging
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+# #incoming
+#   ingress {
+#     from_port        = 80
+#     to_port          = 80
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+# #outging
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "to allow traffic from outside on 80"
-  }
-}
+#   tags = {
+#     Name = "to allow traffic from outside on 80"
+#   }
+# }
 
-#security group for ecs cluster
+# #security group for ecs cluster
 
-resource "aws_security_group_rule" "ecs" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
-  protocol          = "tcp"
-  # cidr_blocks      = ["0.0.0.0/0"]
-  # ipv6_cidr_blocks = ["::/0"]
-  source_security_group_id = aws_security_group.alb_jenkinsterraform.id
-  security_group_id        = aws_security_group.ecs_jenkinsterraform.id
-}
+# resource "aws_security_group_rule" "ecs" {
+#   type              = "ingress"
+#   from_port         = 0
+#   to_port           = 65535
+#   protocol          = "tcp"
+#   # cidr_blocks      = ["0.0.0.0/0"]
+#   # ipv6_cidr_blocks = ["::/0"]
+#   source_security_group_id = aws_security_group.alb_jenkinsterraform.id
+#   security_group_id        = aws_security_group.ecs_jenkinsterraform.id
+# }
 
 
-resource "aws_security_group" "ecs_jenkinsterraform" {
-  name        = "ECS sec group automation"
-  description = "to allow traffic from ALB"
+# resource "aws_security_group" "ecs_jenkinsterraform" {
+#   name        = "ECS sec group automation"
+#   description = "to allow traffic from ALB"
 
-#outging
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+# #outging
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "to allow traffic from ALB"
-  }
-}
+#   tags = {
+#     Name = "to allow traffic from ALB"
+#   }
+# }
