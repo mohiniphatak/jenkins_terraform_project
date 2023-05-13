e78425ff0d6674071dc6199cfa9c95c320e69277
phatakmohini@gmail.com <phatakmohini@gmal.com>
2023-05-13 18:06:52 +0530

    vpc & eks cluster created

--- infra_setup/eks.tf
+++ infra_setup/eks.tf
@@ -34,7 +34,7 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+    subnet_ids = [aws_subnet.example1.id]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
